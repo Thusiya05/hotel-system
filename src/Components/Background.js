dd6981c9b5b98aeef2467b6e7b6c101ff6438b52
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-08 00:25:23 +0530

    add components

---
+++ src/Components/Background.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Background() {
+    return (
+        <div>
+             <div className="animation-area">
+                <ul className="box-area">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
+
