91bcfdc3183903b923f0af2987af64c13d8b1a8d
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-09 02:50:51 +0530

    welcome slide update

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import Banner from "./Banner"
+import {Link} from "react-router-dom" 
 
 export default function Background() {
     return (
         <div>
              <div className="animation-area">
              <div className="default-Hero">
-                    <Banner title="Welcome to Adventure Base Camp"
-                            subtitle="Enjoy the best comforts of an outdoor leisure activity center with a touch of luxury close to nature.
+                    <div className="jumbotron">
+                        <h3>Welcome to Adventure Base Camp,</h3>
+                        <h3>Kitulgala</h3>
+                        <div></div>
+                        <h2>Enjoy the best comforts of an outdoor leisure activity center with a touch of luxury close to nature.
                                     With amazing food, great accommodation and a breath taking location which is one of the largest and best of its kind in South Asia. We have a 
-                                    well experienced and friendly adventure team to customize your days with us."
-                            children="Our Rooms" path="/rooms"></Banner>
-                </div>
+                                    well experienced and friendly adventure team to customize your days with us.</h2>
+                        <Link to="/aboutUs" className="btn btn-outline-info" role="button">More Info</Link>            
+                    </div>
+                    
+                {/* </div> */}
+             </div>
                 <ul className="box-area">
                     <li></li>
                     <li></li>
