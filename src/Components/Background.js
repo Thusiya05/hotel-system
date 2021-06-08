7a133b5acba878069c9d5ecb33b30cdb43ddf140
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-09 00:00:49 +0530

    home page updated

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -4,6 +4,13 @@ export default function Background() {
     return (
         <div>
              <div className="animation-area">
+             <div className="default-Hero">
+                    <Banner title="Welcome to Adventure Base Camp"
+                            subtitle="Enjoy the best comforts of an outdoor leisure activity center with a touch of luxury close to nature.
+                                    With amazing food, great accommodation and a breath taking location which is one of the largest and best of its kind in South Asia. We have a 
+                                    well experienced and friendly adventure team to customize your days with us."
+                            children="Our Rooms" path="/rooms"></Banner>
+                </div>
                 <ul className="box-area">
                     <li></li>
                     <li></li>
