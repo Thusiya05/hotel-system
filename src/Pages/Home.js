1acf9b631f7ff07f0c87f2e6271542749b965f47
Adithya1303 <56554121+Adithya1303@users.noreply.github.com>
2021-06-10 00:52:23 +0530

    new

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from "../Components/Hero"
 import Banner from "../Components/Banner"
 import Background from "../Components/Background"
+import Services from "../Components/Services"
 
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
                 <Banner title="Luxurious rooms" subtitle="Choose your dream room with us." children="Our Rooms" path="/rooms"></Banner>
             </Hero>         
         </div>
+        <Services />
         
         </>
         
