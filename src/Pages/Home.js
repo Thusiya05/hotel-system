3c29d087bdad2cbcb02c9bd5614d3194e5e6fdcd
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-11 20:33:53 +0530

    mobile responsiveness of slider

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
         </div>
         <div className="slider-row">
         <div className="row">
-            <div className="col-6">
+            <div className="col-md-6">
                 <h1>Overview of Adventure Base Camp</h1>
                     <p>The most epic and wonderful place in Kithulgala is now available for guests with safety and disciplines. Are you boring of the busy lifestyle then come to see the nature with lot of adventure and facilities.</p>
 
@@ -26,7 +26,7 @@ export default function Home() {
                     Well qualified and experienced team of instructors for outbound training and activity training</p>
                     <p>* Covid-19 Safety Protocols is followed to ensure a safe & pleasant holiday</p>
             </div>
-            <div className="col-6">
+            <div className="col-md-6">
                 <Slider />
             </div>
         </div>   
