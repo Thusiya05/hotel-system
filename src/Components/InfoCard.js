18c86dbefa315cacca5ccc01b9faf53a4f2b2909
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-20 02:36:53 +0530

    update activity page

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -4,9 +4,9 @@ import {Card} from 'react-bootstrap'
 export default function ActCard({img,title,text}) {
     return (
         <div>
-             <Card style={{ width: '20rem' }}>
+             <Card style={{width:'20rem', margin:'1rem'}}>
                 <Card.Img variant="top" src={img} style={{height:'300px'}} />
-                <Card.Body style={{backgroundColor:"darkgray"}}>
+                <Card.Body style={{backgroundColor:"darkgray", height:'18rem'}}>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{text}</Card.Text>
                 </Card.Body>
