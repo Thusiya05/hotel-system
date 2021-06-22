1d5f993c794976618eb6670916c21033f1117a37
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-22 23:18:15 +0530

    ACTIVITIES

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -5,7 +5,7 @@ export default function ActCard({img,title,text}) {
     return (
         <div>
              <Card style={{width:'20rem', margin:'1rem'}}>
-                <Card.Img variant="top" src={img} style={{height:'300px'}} />
+                <Card.Img variant="top" src={img} style={{height:'300px',width:'20rem'}} />
                 <Card.Body style={{backgroundColor:"darkgray", height:'18rem'}}>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{text}</Card.Text>
