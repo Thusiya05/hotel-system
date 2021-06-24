1e0fedb9715fd6d25dc74d3044eff26c42c75edb
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-24 22:38:24 +0530

    modify3

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -6,8 +6,8 @@ export default function ActCard({img,title,text}) {
         <div>
              <Card style={{width:'20rem', margin:'1rem'}}>
                 <Card.Img variant="top" src={img} style={{height:'300px',width:'19.9rem'}} />
-                <Card.Body style={{backgroundColor:"darkgray", height:'18rem'}}>
-                    <Card.Title>{title}</Card.Title>
+                <Card.Body style={{backgroundColor:"darkgray", height:'18rem',fontFamily:'cursive'}}>
+                    <Card.Title style={{fontWeight:'bolder'}}>{title}</Card.Title>
                     <Card.Text>{text}</Card.Text>
                 </Card.Body>
             </Card> 
--- src/Pages/Facilities.js
+++ src/Pages/Facilities.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
 
 const Facilities = () => {
     return (
-        <div>
-            Hello from Facilities
-        </div>
+        <>
+            <Hero hero="facility-Hero">
+                <Banner title="Just For You" subtitle="Feel the Difference of Our Facilities" children="Return Home" path="/"></Banner>
+            </Hero>
+            <div className="activity-info">
+                <containerFluid>
+                    <h1>Restaurant</h1>
+                </containerFluid>
+            </div>
+        </>
+        
     )
 }
 export default Facilities
