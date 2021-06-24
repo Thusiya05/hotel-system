bd757931b0eb43397913d4301f35615b909dd185
Thusitha karunathilaka <56513558+Thusiya05@users.noreply.github.com>
2021-06-24 13:57:26 +0530

    Revert "activity page modified"

--- src/Pages/Facilities.js
+++ src/Pages/Facilities.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Hero from '../Components/Hero'
-import Banner from '../Components/Banner'
 
 const Facilities = () => {
     return (
-        <>
-            <Hero hero="facility-Hero">
-                <Banner title="Just For You" subtitle="Feel the difference of our Facilities" children="Return home" path="/"></Banner>
-            </Hero>
-        </>
+        <div>
+            Hello from Facilities
+        </div>
     )
 }
 export default Facilities
