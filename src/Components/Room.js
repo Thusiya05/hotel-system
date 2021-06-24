b898319df068ad9f99d55ee38f60008d43110c19
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-06-25 01:15:18 +0530

    btn modified

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -15,8 +15,8 @@ export default function Room({ room }){
           <h6>${price}</h6>
           <p>per night</p>
         </div>
-        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
-          features
+        <Link to={`/rooms/${slug}`} className=" btn btn-outline-primary room-link">
+          Features
         </Link>
 
       </div>
