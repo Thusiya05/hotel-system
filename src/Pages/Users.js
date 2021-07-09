37a1497d0d0cc0f0a7f96afd73cf077c98bdf240
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-07-10 02:15:14 +0530

    Update Users.js

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -17,44 +17,62 @@ const Users =()=>{
                     <br></br>
                     {
                         show?
-                        <div style={{boxShadow:'0 6px 6px gray',padding:'1rem'}}>
+                        <div className="containerFluid" style={{boxShadow:'0 6px 6px gray',padding:'1rem'}}>
                             <Form>
                                 <Form.Row>
                                     <Form.Group as={Col} controlId="formGridFirstName">
-                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Label><h6>First Name</h6></Form.Label>
                                     <Form.Control type="text" />
                                     </Form.Group>
 
                                     <Form.Group as={Col} controlId="formGridLastName">
-                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Label><h6>Last Name</h6></Form.Label>
                                     <Form.Control type="text" />
                                     </Form.Group>
                                 </Form.Row>
                                 <Form.Row>
                                     <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Label>Email</Form.Label>
+                                    <Form.Label><h6>Email</h6></Form.Label>
                                     <Form.Control type="Email" placeholder="Enter Email"/>
                                     </Form.Group>
 
                                     <Form.Group as={Col} controlId="formGridMobile">
-                                    <Form.Label>Mobile No:</Form.Label>
+                                    <Form.Label><h6>Mobile No:</h6></Form.Label>
                                     <Form.Control type="text" placeholder="07x xxx xxx" />
                                     </Form.Group>
                                 </Form.Row>
                                 <Form.Row>
-                                    <Form.Group as={Col} controlId="Gender">
-                                        <div className="col-md-6">
-                                            <Form.Label>Gender:</Form.Label> 
-                                        </div>
-                                        <div classNAme="col-md-6">
-                                        <Col sm={10}>
-                                          <Form.Check  type="radio" label="Male"  name="formHorizontalRadios" id="formHorizontalRadios1" />
-                                        </Col>
-                                                                                                                  
+                                    <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label><h6>Gender</h6></Form.Label>
+                                    <div class="form-check" style={{display:'inline-flex'}}>
+                                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
+                                            <label class="form-check-label" for="exampleRadios1">
+                                                Male
+                                            </label>
+                                            </div>
+                                            <div class="form-check" style={{display:'inline-flex'}}>
+                                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"></input>
+                                            <label class="form-check-label" for="exampleRadios2">
+                                                Fe-Male
+                                            </label>
                                         </div>
                                     </Form.Group>
-                                    <Form.Group as={Col} controlId="Gender">
-                                        
+
+                                    <Form.Group as={Col} controlId="formGridMobile">
+                                    <Form.Label><h6>User Role</h6></Form.Label>
+                                    <Form.Control
+                                                as="select"
+                                                className="my-1 mr-sm-2"
+                                                id="inlineFormCustomSelectPref"
+                                                custom
+                                                
+                                            >
+                                                <option value="0">Choose...</option>
+                                                <option value="1">Receptionist</option>
+                                                <option value="2">Guide</option>
+                                                <option value="3">Stuwart</option>
+                                                <option value="3">Kitchen Staff</option>
+                                        </Form.Control>
                                     </Form.Group>
                                 </Form.Row>
                             </Form>
