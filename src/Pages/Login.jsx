5149cdec52f0538aeabf06dbb1ee7143cdb37e96
darshana782 <56575929+darshana782@users.noreply.github.com>
2021-07-12 01:07:17 +0530

    sign in form - error msg popup - css, login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -103,7 +103,7 @@ export default class Login extends Component {
           case 'firstName': 
             errors.firstName = 
               value.length < 2
-                ? 'Name must be atleast 2 characters long!'
+                ? 'First name must be atleast 2 characters long!'
                 : '';
                 this.setState({firstName: event.target.value})    
             break;
@@ -111,7 +111,7 @@ export default class Login extends Component {
           case 'lastName': 
             errors.lastName = 
               value.length < 2
-                ? 'Name must be atleast 2 characters long!'
+                ? 'Second Name must be atleast 2 characters long!'
                 : '';
                 this.setState({lastName: event.target.value});
             break; 
@@ -128,7 +128,7 @@ export default class Login extends Component {
           var letters = /^[0-9]+$/;
             errors.age = 
             !value.match(letters)
-                ? 'invalid age!'
+                ? 'Invalid Age!'
                 : '';
                 this.setState({age: event.target.value});
             break;
@@ -136,7 +136,7 @@ export default class Login extends Component {
           case 'no': 
             errors.no = 
               value.length < 1
-                ? 'The field must be filled!'
+                ? 'Fields must be filled!'
                 : '';
                 this.setState({no: event.target.value});
             break;
@@ -144,7 +144,7 @@ export default class Login extends Component {
           case 'lane': 
             errors.lane = 
               value.length < 1
-                ? 'The field must be filled!'
+                ? 'Fields must be filled!'
                 : '';
                 this.setState({lane: event.target.value});
             break;
@@ -152,7 +152,7 @@ export default class Login extends Component {
           case 'street': 
             errors.street = 
               value.length < 1
-                ? 'The field must be filled!'
+                ? 'Fields must be filled!'
                 : '';
                 this.setState({street: event.target.value});
             break;
@@ -161,7 +161,7 @@ export default class Login extends Component {
             var letters = /^[0-9]+$/;
               errors.Bdate = 
               !value.match(letters) 
-                 ? "Invalid date!"
+                 ? "Invalid Date!"
                  : '';
                 this.setState({Bdate: event.target.value});
             break;
@@ -170,7 +170,7 @@ export default class Login extends Component {
           var letters = /^[0-9]+$/;
           errors.Bmonth = 
           !value.match(letters) 
-              ? "Invalid month!"
+              ? "Invalid Month!"
               : '';
                 this.setState({Bmonth: event.target.value});
             break;
@@ -179,7 +179,7 @@ export default class Login extends Component {
           var letters = /^[0-9]+$/;
           errors.Byear = 
           !value.match(letters) 
-              ? "Invalid year!"
+              ? "Invalid Year!"
               : '';
                 this.setState({Byear: event.target.value});
             break;
@@ -188,7 +188,7 @@ export default class Login extends Component {
           var letters = /^[0-9Vv]+$/;
             errors.nic = 
             !value.match(letters) 
-                ? "nic don't match!"
+                ? "NIC don't match!"
                 : '';
                 this.setState({nic: event.target.value});
                 
@@ -212,7 +212,7 @@ export default class Login extends Component {
           var letters = /^[0-9]+$/;
             errors.telephoneNumber = 
                !value.match(letters)
-                ? "telephone number don't match!"
+                ? "Telephone number don't match!"
                 : '';
                 this.setState({telephoneNumber: event.target.value});      
             break;          
@@ -232,7 +232,7 @@ export default class Login extends Component {
           if(this.state.confirmPassword!=this.state.password){                
             errors.confirmPassword = "Password don't match!"
             }else{
-            errors.confirmPassword = "Password match!"
+            errors.confirmPassword = "Password matched!"
             }
             break;
           default:
@@ -348,7 +348,7 @@ export default class Login extends Component {
         </NavBar>
             <div className="login">
                 <div className="login-cont">
-                    <div className="login-form login-sign-in">
+                    <div class="loginform" className="login-form login-sign-in">
                         <h2 style={{ color:"black" }}><b>Sign In</b></h2>
                             <label>
                                 <span>Email Address</span>
@@ -387,22 +387,22 @@ export default class Login extends Component {
                         </div>
                         <div className="login-form sign-up">
                             <h2 style={{ color:"black" }}><b>Sign Up</b></h2>
-
+                          <div class="signin_box">
                             <form class="signinform" onSubmit={this.handleSubmit} noValidate>
                                 <label>
                                     <span>First Name</span>
                                     <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} noValidate required />
                                     
                                     {errors.firstName.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}} className='error'>{errors.firstName}</span>}
-
+                                                     <span id="popup" className='error'>{errors.firstName}</span>}
+{/* style={{fontSize:"10px", textTransform:'lowercase', color:'red'}} */}
                                 </label>
                                 <label>
                                     <span>Last Name</span>
                                     <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} noValidate required/>
                                 
                                     {errors.lastName.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.lastName}</span>}
+                                                     <span  id="popup"  className='error'>{errors.lastName}</span>}
                                 
                                 </label>
                                 <label>
@@ -410,7 +410,7 @@ export default class Login extends Component {
                                     <input type="email" name="email" value={this.state.email} onChange={this.handleChange} noValidate required/>
                                 
                                 {errors.email.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.email}</span>}
+                                                     <span  id="popup"  className='error'>{errors.email}</span>}
                                 
                                 </label>
                                 <label>
@@ -418,22 +418,22 @@ export default class Login extends Component {
                                     <input type="text" name="age" maxLength={2} value={this.state.age} onChange={this.handleChange} noValidate required/>
                                 
                                 {errors.age.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.age}</span>}
+                                                     <span  id="popup"  className='error'>{errors.age}</span>}
                                 
                                 </label>
                                 <label>
                                     <span>Address</span>
                                     <input type="text" placeholder="No:" name="no" value={this.state.no} onChange={this.handleChange} noValidate required/>                                   
                                     {errors.no.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.no}</span>}
+                                                        <span  id="popup"  className='error'>{errors.no}</span>}
                                     
                                     <input type="text" placeholder="Lane:" name="lane" value={this.state.lane} onChange={this.handleChange} noValidate  required/>                                    
                                     {errors.lane.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.lane}</span>}
+                                                        <span  id="popup"  className='error'>{errors.lane}</span>}
 
                                     <input type="text" placeholder="Street:" name="street" value={this.state.street} onChange={this.handleChange} noValidate  required/>                                    
                                     {errors.street.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.street}</span>}
+                                                        <span  id="popup"  className='error'>{errors.street}</span>}
                                     
                                 </label>
 
@@ -441,15 +441,15 @@ export default class Login extends Component {
                                     <span>Date of Birth</span>
                                     <input type="text" maxLength={2} placeholder="Date :" name="Bdate" value={this.state.Bdate} onChange={this.handleChange} noValidate  required/>                                   
                                     {errors.Bdate.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.Bdate}</span>}
+                                                        <span  id="popup"  className='error'>{errors.Bdate}</span>}
                                     
                                     <input type="text" maxLength={2} placeholder="Month :" name="Bmonth" value={this.state.Bmonth} onChange={this.handleChange} noValidate required />                                    
                                     {errors.Bmonth.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.Bmonth}</span>}
+                                                        <span  id="popup"  className='error'>{errors.Bmonth}</span>}
 
                                     <input type="text" maxLength={4} placeholder="Year :" name="Byear" value={this.state.Byear} onChange={this.handleChange} noValidate  required/>                                    
                                     {errors.Byear.length > 0 && 
-                                                        <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.Byear}</span>}
+                                                        <span  id="popup"  className='error'>{errors.Byear}</span>}
                                     
                                 </label>
 
@@ -459,7 +459,7 @@ export default class Login extends Component {
                                     <input type="text" name="nic" maxLength={10} value={this.state.nic} onChange={this.handleChange} noValidate  required/>
                                 
                                 {errors.nic.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.nic}</span>}
+                                                     <span  id="popup"  className='error'>{errors.nic}</span>}
                                 
                                 </label>
                                 <label>
@@ -467,7 +467,7 @@ export default class Login extends Component {
                                     <input type="text" name="telephoneNumber" maxLength={10} value={this.state.telephoneNumber} onChange={this.handleChange} noValidate  required/>
                                 
                                 {errors.telephoneNumber.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.telephoneNumber}</span>}
+                                                     <span  id="popup"  className='error'>{errors.telephoneNumber}</span>}
                                 
                                 </label>
                                 <label>
@@ -475,7 +475,7 @@ export default class Login extends Component {
                                     <input type="password" name="password" value={this.state.password} onChange={this.handleChange} noValidate  required/>
                                 
                                 {errors.password.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.password}</span>}
+                                                     <span  id="popup"  className='error'>{errors.password}</span>}
                                 
                                 </label>
                                 <label>
@@ -483,13 +483,14 @@ export default class Login extends Component {
                                     <input type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} noValidate required />
                                 
                                 {errors.confirmPassword.length > 0 && 
-                                                     <span style={{fontSize:"10px", textTransform:'lowercase', color:'red'}}  className='error'>{errors.confirmPassword}</span>}
+                                                     <span  id="popup"  className='error'>{errors.confirmPassword}</span>}
                                 
                                 </label>
                                 <div class="submitButton">
                                     <Button variant="outline-dark" style={{width:"200px"}} className="submit" onClick={this.signup}>Sign Up Now</Button>
                                 </div>
-                            </form>    
+                            </form>  
+                          </div>  
                         </div>
                     </div>
                 </div>
