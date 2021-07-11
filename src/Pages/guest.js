7e04d8eba79a45b6b54b6f81837a557281aa3648
Lakith Kithsara <52199455+LakithKithsara@users.noreply.github.com>
2021-07-11 22:03:43 +0530

    link

---
+++ src/Pages/guest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from '../Components/Sidebar';
+
+function guest() {
+    return (
+        <div className='guest'>
+        <Sidebar/>
+        <h1>
+
+            hello from guest
+        </h1>
+        </div>
+    )
+}
+
+export default guest
