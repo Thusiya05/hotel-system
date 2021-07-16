eba4aa23a70ac8fa9d67b024a5befce07504a157
Lakith Kithsara <52199455+LakithKithsara@users.noreply.github.com>
2021-07-16 22:47:17 +0530

    Update HotelConfig.js

--- src/Pages/HotelConfig.js
+++ src/Pages/HotelConfig.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import Sidebar from '../Components/Sidebar';
+import Title from '../Components/Title';
+import { Button,Form,Col } from 'react-bootstrap';
 
 function HotelConfig() {
     return (
         <div className='HotelConfig'>
         <Sidebar/>
-        <h1>
-
-            hello from Configuration
-        </h1>
-        </div>
+        <Title title="Hotel Configuration"></Title>
+        <div class="text-center">
+        <Button size="lg" href="./income" variant="secondary" 
+            style={{
+                minHeight: "10rem",
+                minWidth: "10rem",
+                padding: "3.5rem 0rem 0rem 0rem",
+                marginTop: "5rem",
+                textAlign: "center",
+                }}>
+            Room Discount
+            </Button> <Button size="lg" href="./expenses" variant="secondary" 
+            style={{
+                minHeight: "10rem",
+                minWidth: "10rem",
+                padding: "3.5rem 0rem 0rem 0rem",
+                marginTop: "5rem",
+                textAlign: "center",
+                }}
+                >Rooms</Button> <Button size="lg" href="./guestreport" variant="secondary" 
+                style={{
+                    minHeight: "10rem",
+                    minWidth: "10rem",
+                    padding: "3.5rem 0rem 0rem 0rem",
+                    marginTop: "5rem",
+                    textAlign: "center",
+                    }}
+                    >Room Type</Button> <Button size="lg" href="./guestreport" variant="secondary" 
+                    style={{
+                        minHeight: "10rem",
+                        minWidth: "10rem",
+                        padding: "3.5rem 0rem 0rem 0rem",
+                        marginTop: "5rem",
+                        textAlign: "center",
+                        }}
+                        >Outdoor Activity</Button>
+                    </div>
+             </div>
+      
             
         
     )
