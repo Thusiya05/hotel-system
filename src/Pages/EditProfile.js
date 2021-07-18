9b6f3b777ac88a14a2aa62c30e3c06764e721db9
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-07-19 02:44:56 +0530

    edit profile

--- src/Pages/EditProfile.js
+++ src/Pages/EditProfile.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
+import Title from '../Components/Title'
+import NavBar from '../Components/NavBar'
 
 const EditProfile=()=>{
     return(
         <>
-
+            <div>
+                <NavBar path1="/" name1="Home" path2="/rooms/" name2="Rooms" path3="/activities" name3="Activities" path4="/facilities" name4="Facilities" path5="/aboutUs" name5="About Us" pathSign="/signIn" LogName="Log In"></NavBar>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            
+            <div style={{background:'linear-gradient(90deg, #c7c4bd 0%, #e4e2dd 50%, #faf9f8 100%'}}>
+                    <br></br>
+                    <br></br>
+                    
+            <Container style={{boxShadow:'1px 2px 6px 1px gray',width:'44rem',borderRadius:'2%',padding:'1rem'}}>
+                    <div className="row">
+                        <h3>Edit Profile. . .</h3>  
+                    </div>
+            </Container>
+            </div>
+           
         </>
     )
 }
