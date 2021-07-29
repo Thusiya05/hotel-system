398397361fae866b37aa0c8019e222e25d1012df
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-07-30 01:10:55 +0530

    my booking

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,47 +3,46 @@ import logo from "../images/logo.png"
 import { Navbar,Nav,Modal,Container,Row,Col,NavDropdown } from 'react-bootstrap'
 import {FaUserCircle} from 'react-icons/fa'
 
-function MyBooking(props){
-    return(
-      <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
+// function MyBooking(props){
+//     return(
+//       <Modal
+//       {...props}
+//       size="lg"
+//       aria-labelledby="contained-modal-title-vcenter"
       
-    >
-    <Modal.Header closeButton style={{backgroundColor:'lightgray'}}>
-      <Modal.Title id="contained-modal-title-vcenter">
-        Charges
-      </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Container>
-              <Row>
-                  <Col sm={6}>
-                      <h6>Room Charge </h6>
-                      <h6>Activity Charges </h6>
-                      <h6>Tax</h6>
-                      <hr></hr>
-                      <h6>Total</h6>
-                  </Col>
-                  <Col sm={6}>
-                      <h6>13 500 </h6>
-                      <h6>5000 </h6>
-                      <h6>1000</h6>
-                      <hr></hr>
-                      <h6>19500</h6>
-                  </Col>
-                </Row>
-              </Container>
-            </Modal.Body>
-            <Modal.Footer style={{backgroundColor:'lightgray'}}>
-                Adventure Base Camp, Kitulgala.
-            </Modal.Footer>
-    </Modal>
-    );
-}
+//     >
+//     <Modal.Header closeButton style={{backgroundColor:'lightgray'}}>
+//       <Modal.Title id="contained-modal-title-vcenter">
+//         Charges
+//       </Modal.Title>
+//             </Modal.Header>
+//             <Modal.Body>
+//               <Container>
+//               <Row>
+//                   <Col sm={6}>
+//                       <h6>Room Charge </h6>
+//                       <h6>Activity Charges </h6>
+//                       <h6>Tax</h6>
+//                       <hr></hr>
+//                       <h6>Total</h6>
+//                   </Col>
+//                   <Col sm={6}>
+//                       <h6>13 500 </h6>
+//                       <h6>5000 </h6>
+//                       <h6>1000</h6>
+//                       <hr></hr>
+//                       <h6>19500</h6>
+//                   </Col>
+//                 </Row>
+//               </Container>
+//             </Modal.Body>
+//             <Modal.Footer style={{backgroundColor:'lightgray'}}>
+//                 Adventure Base Camp, Kitulgala.
+//             </Modal.Footer>
+//     </Modal>
+//     );
+// }
 export default function NavBar({path1,name1,path2,name2,path3,name3,path4,name4,path5,name5,pathSign,LogName}){
-        const[open,setOpen]=useState(false);
        return(
          <>
            <Navbar className="new3" collapseOnSelect expand="lg" bg="light" variant="dark" fixed="top">
@@ -63,7 +62,7 @@ export default function NavBar({path1,name1,path2,name2,path3,name3,path4,name4,
                     <NavDropdown.Item href="/signIn"> {LogName}</NavDropdown.Item>
                     <NavDropdown.Item href="/EditProfile">Edit Profile</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">My Bookings</NavDropdown.Item>
+                    <NavDropdown.Item href="/MyBookings">My Bookings</NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
                   <Nav>
@@ -74,10 +73,10 @@ export default function NavBar({path1,name1,path2,name2,path3,name3,path4,name4,
                   </Nav>
               </Navbar.Collapse>
             </Navbar>
-            <MyBooking 
+            {/* <MyBooking 
               show={open}
               onHide={()=>setOpen(false)}
-            />
+            /> */}
          </>
            
        )
