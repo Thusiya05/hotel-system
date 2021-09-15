ac37adb32a6b5fff01f8e9773d4f5d56da3aae27
darshana782 <56575929+darshana782@users.noreply.github.com>
2021-09-15 14:50:20 +0530

    customizable food menu with cart for confirm order

--- src/Pages/Customer/cusfoodmenu.js
+++ src/Pages/Customer/cusfoodmenu.js
@@ -10,8 +10,82 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import "../../CSS/Cusfoodmenu.css";
 
+function ConfirmOrder(props){
+    console.log(props.IdOfOrderedFoods);
+    console.log(props.QtyOfOrderedFoods);
+    console.log(props.NameOfOrderedFoods);
+
+    const foodName = props.NameOfOrderedFoods;
+    const food_name = foodName.map((x) => 
+        <p>{x}</p>
+    );
+
+    const qty = props.QtyOfOrderedFoods;
+    const food_qty = qty.map((y) => 
+        <p>{y}</p>
+    );
+    // console.log(QtyArray);
+    return(
+        <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton style={{backgroundColor:'lightgray'}}>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Please Confirm your Order
+          </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+
+                        {/* <Row>
+                            <Col md={4}></Col> 
+                            <Col md={4}>
+                                <Form.Group as={Col} controlId="editDiscountName">
+                                <Form.Label style={{textAlign:'left'}}><h6 >{food_name} {food_qty}</h6></Form.Label>
+                                </Form.Group>
+                            </Col>             
+                        </Row> */}
+
+                        <Table striped bordered hover size="sxm">
+                            <thead>
+                                <tr>
+                                    <th>Food Name</th>
+                                    <th>Qty</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{food_name}</td>
+                                    <td>{food_qty}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+
+                        <div style={{textAlign:'center'}}>
+                            <Button type="submit" variant="info">Confirm Order</Button> 
+                            <Button onClick={props.onHide} variant="danger">Cancel Order</Button>
+                        </div> 
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer style={{backgroundColor:'lightgray'}}>
+                    Adventure Base Camp, Kitulgala.
+                </Modal.Footer>
+            </Modal>
+    );
+}
+
+
+
+
+
 
 const Cusfoodmenu =()=> {
+    const [IdOfOrderedFoods, setIdOfOrderedFoods]=useState([])
+    const [QtyOfOrderedFoods, setQtyOfOrderedFoods]=useState([])
+    const [NameOfOrderedFoods, setNameOfOrderedFoods]=useState([])
     
     const [editshow,setEditShow]=useState(false)
     const[foods,setFood]=useState([]);
@@ -19,12 +93,12 @@ const Cusfoodmenu =()=> {
     const [added, setadded] = useState(true);
     const[editFood,setEditFood]=useState(1);
 
-    function Update(foodId){
-        // console.log(foodId)
-        setadded(!added);
-        setEditView(true);
-        setEditFood(foodId);
-    }
+    // function Update(foodId){
+    //     // console.log(foodId)
+    //     setadded(!added);
+    //     setEditView(true);
+    //     setEditFood(foodId);
+    // }
 
     useEffect(() => {
         axios.get('http://localhost:3030/cusfoodmenu')
@@ -36,23 +110,46 @@ const Cusfoodmenu =()=> {
         })
     },[])
 
+    function submit(){
+        let foodIdArray = [];
+        let foodNameArray = [];
+        let QtyArray = [];
+        foods.forEach(test => {
+            if(test.value>0){
+                foodIdArray.push(test.foodId);
+                foodNameArray.push(test.foodName);
+                QtyArray.push(test.value);
+            }
+
+                setIdOfOrderedFoods(foodIdArray);
+                setQtyOfOrderedFoods(QtyArray);
+                setNameOfOrderedFoods(foodNameArray);
+            })
+        // console.log(foodIdArray);
+        // console.log(QtyArray);
+        setEditView(true);
+    }
 
     return (
         <>      
-                
+        {/* {...props}   */}
         {
              <div>
              <h4 style={{textAlign:'center',fontFamily:'monospace'}}>Food Menu.</h4>
 
+             <div style={{textAlign:'center'}}>
+                        <Button type="button" onClick={() => submit()} variant="info">Place Order</Button> 
+                        {/* <Button onClick={props.onHide} variant="danger">Cancel</Button> */}
+             </div>
+
              <br></br>
              <br></br>
 
-             {/* <Form onSubmit={(e) => submit(e)}> */}
              <Table striped bordered hover size="sm" responsive id="CheckInTable">
                  <thead>
                      <tr>
-                         <th>#</th>
                          <th>Food Name</th>
+                         <th>Price</th>
                          <th> </th>
                      </tr>
                  </thead>
@@ -61,48 +158,50 @@ const Cusfoodmenu =()=> {
                          foods.map(
                              test=>
                              <tr key = {test.foodId}>
-                                 <td>{test.foodId}</td>
                                  <td>{test.foodName}</td>
+                                 <td>{test.price}</td>
                                  <td style={{textAlign:'center'}}>
                                     {/* <Tippy content="Minus">
                                         <Button onClick={()=>Update(test.foodId)} type="minus"><FaMinus /></Button>
                                     </Tippy> */}
                                     <input type="number" min="0" id={test.foodId}
-                                        // onChange={e=>{ 
-                                        //     let value=e.target.value;
-                                        //     setFood(
-                                        //         foods.map(sd=>{
-                                        //             if(sd.foodId==e.target.id){
-                                        //                 sd.value=value;
-                                        //             }
-                                        //             return sd;
-                                        //         })
-                                        //     );
-                                        // }}
+                                        onChange={e=>{
+                                            let value=e.target.value;
+                                            setFood(
+                                                foods.map(sd=>{
+                                                    if(sd.foodId==e.target.id){
+                                                        sd.value=value;
+                                                    }
+                                                    return sd;
+
+                                                })
+                                            );
+                                        }}
                                     ></input>
-                                    {/* <Tippy content="Add">
-                                        <Button onClick={()=>Update(test.foodId)} type="add"><FaPlus /></Button>
-                                    </Tippy> */}
                                 </td>
                              </tr>
                          )
                      }
                  </tbody>
-{/* 
-                 <div style={{textAlign:'center'}}>
-                        <Button type="submit" variant="info">Place Order</Button> 
-                        <Button onClick={props.onHide} variant="danger">Cancel</Button>
-                 </div> */}
+
+                
              </Table>
-             {/* </Form> */}
-             {/* <Editfood
+             <ConfirmOrder
                          show={editView}
                          onHide={()=> setEditView(false)} 
-                         editFood={editFood}
-                         setEditFood={setEditFood}
+
+                         QtyOfOrderedFoods={QtyOfOrderedFoods}
+                         setQtyOfOrderedFoods={setQtyOfOrderedFoods}
+
+                         IdOfOrderedFoods={IdOfOrderedFoods}
+                         setIdOfOrderedFoods={setIdOfOrderedFoods}
+
+                         NameOfOrderedFoods={NameOfOrderedFoods}
+                         setNameOfOrderedFoods={setNameOfOrderedFoods}
+
                          added={added}
                          setadded={setadded}
-                     /> */}
+                     />
             </div> 
             // :null
          }
